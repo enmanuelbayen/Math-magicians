01f488efdefed962814d62de03e4d3824111a846
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-30 13:03:04 -0400

    Style of calculator html

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Keys from './Keys';
+
+const Calculator = () => (
+  <div className="calculator">
+    <div className="result">0</div>
+    <Keys />
+  </div>
+);
+
+export default Calculator;
