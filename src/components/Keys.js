3948603b61a2245eb469e14b4cbbcce27e197cb1
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-31 15:48:22 -0400

    Add first setup for events on Calculator
    Install npm propType and add Logic folder

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import '../Calculator.css';
+import PropTypes from 'prop-types';
 
-const Keys = () => (
-  <div className="key-container grid">
-    <button className="key-row" type="button">AC</button>
-    <button className="key-row" type="button">+/-</button>
-    <button className="key-row" type="button">%</button>
-    <button className="key-row orange" type="button">+</button>
-    <button className="key-row" type="button">7</button>
-    <button className="key-row" type="button">8</button>
-    <button className="key-row" type="button">9</button>
-    <button className="key-row orange" type="button">x</button>
-    <button className="key-row" type="button">4</button>
-    <button className="key-row" type="button">5</button>
-    <button className="key-row" type="button">6</button>
-    <button className="key-row orange" type="button">-</button>
-    <button className="key-row" type="button">1</button>
-    <button className="key-row" type="button">2</button>
-    <button className="key-row" type="button">3</button>
-    <button className="key-row orange" type="button">+</button>
-    <button className="key-row" id="ceroKey" type="button">0</button>
-    <button className="key-row" type="button">.</button>
-    <button className="key-row orange" type="button">=</button>
-  </div>
-);
+const Keys = ({ onClick }) => {
+  const handleClick = (buttonName) => {
+    onClick(buttonName);
+  };
+
+  return (
+    <div className="key-container grid">
+      <button className="key-row" type="button" onClick={() => handleClick('AC')}>AC</button>
+      <button className="key-row" type="button" onClick={() => handleClick('+/-')}>+/-</button>
+      <button className="key-row" type="button" onClick={() => handleClick('%')}>%</button>
+      <button className="key-row orange" type="button" onClick={() => handleClick('÷')}>÷</button>
+      <button className="key-row" type="button" onClick={() => handleClick('7')}>7</button>
+      <button className="key-row" type="button" onClick={() => handleClick('8')}>8</button>
+      <button className="key-row" type="button" onClick={() => handleClick('9')}>9</button>
+      <button className="key-row orange" type="button" onClick={() => handleClick('x')}>x</button>
+      <button className="key-row" type="button" onClick={() => handleClick('4')}>4</button>
+      <button className="key-row" type="button" onClick={() => handleClick('5')}>5</button>
+      <button className="key-row" type="button" onClick={() => handleClick('6')}>6</button>
+      <button className="key-row orange" type="button" onClick={() => handleClick('-')}>-</button>
+      <button className="key-row" type="button" onClick={() => handleClick('1')}>1</button>
+      <button className="key-row" type="button" onClick={() => handleClick('2')}>2</button>
+      <button className="key-row" type="button" onClick={() => handleClick('3')}>3</button>
+      <button className="key-row orange" type="button" onClick={() => handleClick('+')}>+</button>
+      <button className="key-row" id="ceroKey" type="button" onClick={() => handleClick('0')}>0</button>
+      <button className="key-row" type="button" onClick={() => handleClick('.')}>.</button>
+      <button className="key-row orange" type="button" onClick={() => handleClick('=')}>=</button>
+    </div>
+  );
+};
+
+Keys.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Keys;
