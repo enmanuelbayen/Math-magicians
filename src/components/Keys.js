b919c7e0f53e9e20669bd7965d3e698cb90746c8
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-31 16:03:00 -0400

    Fix prop error
    Add operation from calculator

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Keys = ({ onClick }) => {
-  const handleClick = (buttonName) => {
-    onClick(buttonName);
-  };
-
-  return (
-    <div className="key-container grid">
-      <button className="key-row" type="button" onClick={() => handleClick('AC')}>AC</button>
-      <button className="key-row" type="button" onClick={() => handleClick('+/-')}>+/-</button>
-      <button className="key-row" type="button" onClick={() => handleClick('%')}>%</button>
-      <button className="key-row orange" type="button" onClick={() => handleClick('÷')}>÷</button>
-      <button className="key-row" type="button" onClick={() => handleClick('7')}>7</button>
-      <button className="key-row" type="button" onClick={() => handleClick('8')}>8</button>
-      <button className="key-row" type="button" onClick={() => handleClick('9')}>9</button>
-      <button className="key-row orange" type="button" onClick={() => handleClick('x')}>x</button>
-      <button className="key-row" type="button" onClick={() => handleClick('4')}>4</button>
-      <button className="key-row" type="button" onClick={() => handleClick('5')}>5</button>
-      <button className="key-row" type="button" onClick={() => handleClick('6')}>6</button>
-      <button className="key-row orange" type="button" onClick={() => handleClick('-')}>-</button>
-      <button className="key-row" type="button" onClick={() => handleClick('1')}>1</button>
-      <button className="key-row" type="button" onClick={() => handleClick('2')}>2</button>
-      <button className="key-row" type="button" onClick={() => handleClick('3')}>3</button>
-      <button className="key-row orange" type="button" onClick={() => handleClick('+')}>+</button>
-      <button className="key-row" id="ceroKey" type="button" onClick={() => handleClick('0')}>0</button>
-      <button className="key-row" type="button" onClick={() => handleClick('.')}>.</button>
-      <button className="key-row orange" type="button" onClick={() => handleClick('=')}>=</button>
-    </div>
-  );
-};
+const Keys = ({ onClick }) => (
+  <div className="key-container grid">
+    <button className="key-row" type="button" onClick={() => onClick('AC')}>AC</button>
+    <button className="key-row" type="button" onClick={() => onClick('+/-')}>+/-</button>
+    <button className="key-row" type="button" onClick={() => onClick('%')}>%</button>
+    <button className="key-row orange" type="button" onClick={() => onClick('÷')}>÷</button>
+    <button className="key-row" type="button" onClick={() => onClick('7')}>7</button>
+    <button className="key-row" type="button" onClick={() => onClick('8')}>8</button>
+    <button className="key-row" type="button" onClick={() => onClick('9')}>9</button>
+    <button className="key-row orange" type="button" onClick={() => onClick('x')}>x</button>
+    <button className="key-row" type="button" onClick={() => onClick('4')}>4</button>
+    <button className="key-row" type="button" onClick={() => onClick('5')}>5</button>
+    <button className="key-row" type="button" onClick={() => onClick('6')}>6</button>
+    <button className="key-row orange" type="button" onClick={() => onClick('-')}>-</button>
+    <button className="key-row" type="button" onClick={() => onClick('1')}>1</button>
+    <button className="key-row" type="button" onClick={() => onClick('2')}>2</button>
+    <button className="key-row" type="button" onClick={() => onClick('3')}>3</button>
+    <button className="key-row orange" type="button" onClick={() => onClick('+')}>+</button>
+    <button className="key-row" id="ceroKey" type="button" onClick={() => onClick('0')}>0</button>
+    <button className="key-row" type="button" onClick={() => onClick('.')}>.</button>
+    <button className="key-row orange" type="button" onClick={() => onClick('=')}>=</button>
+  </div>
+);
 
 Keys.propTypes = {
   onClick: PropTypes.func.isRequired,
