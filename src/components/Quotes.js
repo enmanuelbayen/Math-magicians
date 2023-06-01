7e8b3256e10a2a12d68e25f1745ad78e9f9e3dab
enmanuelbayen <enmanuel0495@gmail.com>
2023-06-01 12:04:43 -0400

    Create Quote file
    Api that generate random quotes

---
+++ src/components/Quotes.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+
+const Quotes = () => {
+    const [quotes, setQuotes] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchQuote = async () => {
+          setLoading(true);
+          try {
+            const response = await fetch(
+              `https://api.api-ninjas.com/v1/quotes?category=${'learning'}`,
+              {
+                headers: {
+                  'X-Api-Key': '4cIYolx2izdKXSCL2c8Ow36G7YHL1z8gflMZ7nkt',
+                },
+              },
+            );
+            const json = await response.json();
+            setQuotes(json[0]);
+          } catch (error) {
+            setError(true);
+          }
+          setLoading(false);
+        };
+        fetchQuote();
+      }, []);
+    
+      if (error) {
+        return (
+          <p className="quoteError">Ups... Quote is missing!</p>
+        );
+      }
+      if (loading) {
+        return (
+          <p className="quoteLoading">Loading...</p>
+        );
+      }
+
+    return (
+        <div className='quotesBox'>
+            {quotes && (
+                <p className='dailyQuote'>
+                    &quot;
+                    {quotes.quote}
+                    {' '}
+                    &quot; -
+                    <span>
+                    {' '}
+                    {quotes.author}
+                    </span>
+                </p>
+            )}     
+        </div>
+    );
+};
+
+export default Quotes;
